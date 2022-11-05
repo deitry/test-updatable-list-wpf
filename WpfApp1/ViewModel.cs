5686f712b2c640e3a6011c40ff28cdf5839954a0
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2022-11-05 20:34:21 +0100

    Remove backing field for ListView items

--- WpfApp1/ViewModel.cs
+++ WpfApp1/ViewModel.cs
@@ -15,10 +15,9 @@ public sealed class ViewModel : INotifyPropertyChanged
         DataManagerInstance = (Application.Current as App)!.DataManagerInstance;
     }
 
-    public List<string> MyItems => _myItems;
+    public List<string> MyListItems => DataManagerInstance.GetListByChar(_mySelectedChar);
     public List<char> AvailableComboBoxItems => DataManagerInstance.AvailableComboBoxItems;
 
-    private List<string> _myItems = new List<string> { "initialized", "default", "values" };
     private char _mySelectedChar = DataManager.NotAValue;
     private readonly DataManager DataManagerInstance;
 
@@ -28,8 +27,7 @@ public sealed class ViewModel : INotifyPropertyChanged
         set
         {
             _mySelectedChar = value;
-
-            SetField(ref _myItems, DataManagerInstance.GetListByChar(value), nameof(MyItems));
+            OnPropertyChanged(nameof(MyListItems));
         }
     }
 
@@ -38,13 +36,5 @@ public sealed class ViewModel : INotifyPropertyChanged
     private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
     }
 }
